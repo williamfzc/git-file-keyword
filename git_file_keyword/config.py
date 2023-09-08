e49d290f21378faa54a58f68b48a395bbbeaef2b
williamfzc <178894043@qq.com>
2023-09-09 00:03:10 +0800

    chore: ignore dir rather than raise error

--- git_file_keyword/config.py
+++ git_file_keyword/config.py
@@ -40,8 +40,10 @@ class ExtractConfig(BaseModel):
         self.repo_root = root
 
         real_file_list = [pathlib.Path(each_file) for each_file in self.file_list]
+
+        final = []
         for each_file in real_file_list:
-            if not (root / each_file).exists():
-                return FileNotFoundError(each_file)
-        self.file_list = real_file_list
+            if (root / each_file).is_file():
+                final.append(each_file)
+        self.file_list = final
         return None
