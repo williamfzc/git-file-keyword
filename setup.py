10d1230abb7a66b0c192a6a73adfde062c6e1701
williamfzc <178894043@qq.com>
2023-09-10 22:21:35 +0800

    fix: security issue in GitPython

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     ],
     install_requires=[
         "pydantic==2.3.0",
-        "GitPython==3.1.27",
+        "GitPython==3.1.35",
         "scikit-learn==1.3.0",
         "loguru==0.7.1",
         "click==8.1.3",
