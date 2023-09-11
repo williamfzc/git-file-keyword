4686c2e4ba34c22a1e56eda7869976e12322cb23
williamfzc <178894043@qq.com>
2023-09-12 00:05:54 +0800

    chore: example update

--- example/stopword_extractor.py
+++ example/stopword_extractor.py
@@ -1,12 +1,17 @@
-import json
-
+"""
+This script is usually used for extracting stopwords from global word dict.
+"""
 from git_file_keyword.config import ExtractConfig
 from git_file_keyword.extractor import Extractor
 
 config = ExtractConfig()
 config.repo_root = ".."
+config.cache_enabled = False
+
 extractor = Extractor(config)
 result = extractor.extract()
-output = result.export_global_word_freq()
-with open("output.json", "w+") as f:
-    json.dump(result.export_global_word_freq(), f)
+
+items = result.export_global_word_freq().items()
+sorted_items = sorted(items, key=lambda x: x[1], reverse=True)
+for item in sorted_items:
+    print(item[0], item[1])
